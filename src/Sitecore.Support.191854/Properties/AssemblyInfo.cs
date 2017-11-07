95a815413840d0a8c8c42e62577a8c8aae89813b
DK\MVO <mvo@sitecore.net>
2017-11-07 14:54:48 +0200

    fixes the value of "defaultContentDatabaseName" for XM single

---
+++ src/Sitecore.Support.191854/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.191854")]
+[assembly: AssemblyProduct("Sitecore.Support.191854")]
+[assembly: ComVisible(false)]
